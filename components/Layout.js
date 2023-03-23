71c705891d3880cba77709d4580b6b26ad086fb7
ThinnThazinWin <thinnthazinwin528@gmail.com>
2023-03-23 19:59:43 +0630

    initial

--- components/Layout.js
+++ components/Layout.js
@@ -5,13 +5,13 @@ export default function Layout({ children }) {
     <div className="layout">
       <header>
         <Link href="/">
-          <a>
+          
             <h1>
               <span>Just Add</span>
               <span>Marmite</span>
             </h1>
             <h2>Spread The Joy</h2>
-          </a>
+          
         </Link>
       </header>
 
@@ -20,7 +20,7 @@ export default function Layout({ children }) {
       </div>
 
       <footer>
-        <p>Copyright 2021 Just Add Marmite :)</p>
+        <p>Copyright 2021 Just Add Marmite :</p>
       </footer>
     </div>
   )
